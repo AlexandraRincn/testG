9bb009afda2844a67ed4a0905f7dfefa6efb948a
Alexandra Rincon Mora <116049210+AlexandraRincn@users.noreply.github.com>
2023-08-17 09:33:35 -0400

    Update Google_Automatizacion.cy.js

--- CYPRESS/cypress/e2e/Google_Automatizacion.cy.js
+++ CYPRESS/cypress/e2e/Google_Automatizacion.cy.js
@@ -4,7 +4,9 @@ describe('Automatización en Cypress', () =>{
     it("Find the contents Año de primera automatizacion desde Cypress", () =>{
         cy.log("Bienvenido a Cypress")
         cy.visit('https://www.google.com/')
-        cy.get('[name="q"]').should("be.visible")
+        cy.get('#L2AGLb > dic').click()
+        cy.get('[name="q"]').should("be.visible").type("automatizacion{enter}")
+        cy.contains('wikipedia.org').click()        
     
     })
        })
