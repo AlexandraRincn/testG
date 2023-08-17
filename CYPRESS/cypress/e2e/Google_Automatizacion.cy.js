f2b7bd10b89c318b0da7df3fdab0b46d240828eb
alexandrarincn <alexandrarincn18@gmail.com>
2023-08-17 05:39:14 -0400

    changes

--- CYPRESS/cypress/e2e/Google_Automatizacion.cy.js
+++ CYPRESS/cypress/e2e/Google_Automatizacion.cy.js
@@ -4,7 +4,7 @@ describe('Automatización en Cypress', () =>{
     it("Find the contents Año de primera automatizacion desde Cypress", () =>{
         cy.log("Bienvenido a Cypress")
         cy.visit('https://www.google.com/')
-        cy.get('[name="q"]').should("be.visible").type("automatizacion")
+        cy.get('[name="q"]').should("be.visible")
     
     })
        })
